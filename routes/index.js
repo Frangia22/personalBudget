e30a18ff911922aa1bc0be9e3d1bf4e7939e6fdc
Frangia22 <giacherofranc22@gmail.com>
2021-07-28 22:43:40 -0300

    Admin listo(Read)

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,44 @@ const api = require('../api');
 router.get('/', (req, res) => {
   res.render('index', { title: 'Personal budget'});
 });
+/* Admin panel */
+router.get('/adminBudget', async (req, res) => {
+  const budgets = await api.getBudgets();
+  res.render('pages/adminBudget', { title: 'Admin panel', budgets});
+});
+/* Add operation GET */
+router.get('/addBudget', (req, res) => {
+  res.render('pages/addBudget', { title: 'Add operation'});
+});
+/* Add operation POST */
+router.post('/add', async (req, res) => {
+  const {concept, amount, date, type, reference} = req.body
+  await api.addBudget(concept, amount, date, type, reference);
+  console.log(req.body);
+  res.redirect('/adminBudget');
+});
+/* Delete operation GET */
+router.get('/delete/:id', async (req, res) => {
+  const affectedRows = await api.deleteBudget(req.params.id);
+  if (affectedRows > 0) {
+    res.redirect('/adminBudget');
+  }else{
+    res.send('Algo salio mal en el proceso de eliminación');
+  }  
+});
+/* Edit operation GET */
+router.get('/edit/:id', async (req, res) => {
+  const budget = await api.getBudgetById(req.params.id)
+  res.render('pages/editBudget', { title: 'Edit operation', budget});
+});
+/* Edit operation POST */
+router.post('/edit/:id', async (req, res) => {
+  const {concept, amount, type, reference} = req.body
+  const id = req.params.id;
+  await api.updateBudget(id, concept, amount, type, reference);
+  console.log(req.body);
+  res.redirect('/adminBudget');
+});
+
 
 module.exports = router;
